263240b6f688a7aa6032e0deecda1fa12201d098
Vikas Kannurpatti Jayaram <vikas.ramasethu@gmail.com>
2012-09-03 13:21:57 +1000

    Update src/org/java_websocket/drafts/Draft_17.java
    Accepts the previous version of Draft_17 and the new Draft_17 
    which accepts subprotocol and origin.

--- src/org/java_websocket/drafts/Draft_17.java
+++ src/org/java_websocket/drafts/Draft_17.java
@@ -8,7 +8,10 @@ public class Draft_17 extends Draft_10 {
 	private String subProtocol;
 	private String origin;
 	
-	public Draft_17() {}
+	public Draft_17() {
+		this.subProtocol = null;
+		this.origin = null;
+	}
 	public Draft_17(String subProtocol, String origin) {
 		this.subProtocol = subProtocol;
 		this.origin = origin;
@@ -24,8 +27,12 @@ public class Draft_17 extends Draft_10 {
 	@Override
 	public ClientHandshakeBuilder postProcessHandshakeRequestAsClient( ClientHandshakeBuilder request ) {
 		super.postProcessHandshakeRequestAsClient( request );
-		request.put("Sec-WebSocket-Protocol", subProtocol);
-		request.put("Origin", origin);
+		if(subProtocol != null) {
+			request.put("Sec-WebSocket-Protocol", subProtocol);			
+		}
+		if(origin != null) {
+			request.put("Origin", origin);			
+		}
 		request.put( "Sec-WebSocket-Version", "13" );// overwriting the previous
 		return request;
 	}
